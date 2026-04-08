52fe5e07a8324f07559b94d9df784a78d58aa2ea
angel47740 <angel47740@gmail.com>
2026-04-08 15:33:15 +0800

    style:調整Hero

--- src/components/HeroCom.vue
+++ src/components/HeroCom.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const line1Left = 'UI/UX '
-const line1Right = 'Designer'
-const line2Left = 'Front-end '
-const line2Right = 'Developer'
+const line1Left = 'Front-end '
+const line1Right = 'Developer'
+const line2Left = '& UI/UX '
+const line2Right = 'Designer'
+const line3 = 'Specializing in Vue.js and Responsive Web Design.'
 
 const line1LeftDisplay = ref('')
 const line1RightDisplay = ref('')
 const line2LeftDisplay = ref('')
 const line2RightDisplay = ref('')
+const line3Display = ref('')
 
 onMounted(async () => {
   await delay(1000)
@@ -18,6 +20,8 @@ onMounted(async () => {
   await delay(300)
   await typeText(line2Left, line2LeftDisplay)
   await typeText(line2Right, line2RightDisplay)
+  await delay(300)
+  await typeText(line3, line3Display)
 })
 
 // 工具函式
@@ -45,7 +49,7 @@ function delay(ms) {
     class="relative flex min-h-screen flex-col items-center justify-center bg-white px-4 text-gray-900 md:px-20"
   >
     <h1
-      class="absolute bottom-5 left-5 z-20 text-[34px] leading-tight font-light md:bottom-10 md:left-10 md:text-[100px]"
+      class="absolute bottom-5 left-5 z-20 text-[34px] leading-tight font-medium md:bottom-10 md:left-10 md:text-[94px]"
     >
       <div>
         {{ line1LeftDisplay }}
@@ -54,11 +58,22 @@ function delay(ms) {
 
       <div class="relative">
         <!-- 占位符：預留正確空間 -->
-        <div class="invisible"><span class="text-[#82ddcf]">Front-end</span> Developer</div>
+        <div class="invisible"><span class="text-[#82ddcf]">& UI/UX </span> Designer</div>
         <!-- 實際文字 -->
         <div class="absolute top-0 left-0">
           <span class="text-[#82ddcf]">{{ line2LeftDisplay }}</span
           ><span class="text-gray-900">{{ line2RightDisplay }}</span>
+        </div>
+      </div>
+
+      <div class="relative mt-2.5 font-sans text-[18px] md:text-[40px]">
+        <!-- 占位符：預留正確空間 -->
+        <div class="invisible">
+          <span class="text-[#82ddcf]">Specializing in Vue.js and Responsive Web Design.</span>
+        </div>
+        <!-- 實際文字 -->
+        <div class="absolute top-0 left-0">
+          <span>{{ line3Display }}</span>
         </div>
       </div>
     </h1>
@@ -79,6 +94,15 @@ function delay(ms) {
         <h2 class="text-[28px] leading-tight font-bold md:text-5xl">
           Wen Yi <span class="text-[#ffd818]">Hsu</span>
         </h2>
+        <button>
+          <a
+            href="/files/WenYi_Hsu_Resume.pdf"
+            target="_blank"
+            class="mt-4 inline-block rounded bg-[#82ddcf] px-4 py-2 text-sm font-medium text-white transition-colors duration-300 hover:bg-[#5ac1b8]"
+          >
+            View Resume
+          </a>
+        </button>
       </div>
     </div>
   </section>
